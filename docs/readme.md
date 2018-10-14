624d22ba7be9312b672023a55ea7ea814c95f6f2
rahuldahake <rahuldahake>
2018-10-14 20:58:57 +0530

    JWT Auth Implemented

---
+++ docs/readme.md
@@ -0,0 +1,9 @@
+# 	ngx-boilerplate
+
+Welcome to the project documentation!
+
+Use `npm run docs` for easier navigation.
+
+## Available documentation
+
+[[index]]
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,54 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
+import { RouteReuseStrategy, RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { RouteReusableStrategy } from './route-reusable-strategy';
+import { I18nService } from './i18n.service';
+import { HttpService } from './http/http.service';
+import { HttpCacheService } from './http/http-cache.service';
+import { ApiPrefixInterceptor } from './http/api-prefix.interceptor';
+import { ErrorHandlerInterceptor } from './http/error-handler.interceptor';
+import { CacheInterceptor } from './http/cache.interceptor';
+
+// JWT and Error Interceptor
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+
+@NgModule({
+  imports: [CommonModule, HttpClientModule, TranslateModule, RouterModule],
+  providers: [
+    I18nService,
+    HttpCacheService,
+    ApiPrefixInterceptor,
+    ErrorHandlerInterceptor,
+    CacheInterceptor,
+    {
+      provide: HttpClient,
+      useClass: HttpService
+    },
+    {
+      provide: RouteReuseStrategy,
+      useClass: RouteReusableStrategy
+    },
+    { 
+      provide: HTTP_INTERCEPTORS, 
+      useClass: JwtInterceptor, multi: true 
+    },
+    { 
+      provide: HTTP_INTERCEPTORS, 
+      useClass: ErrorInterceptor, multi: true 
+    },
+  ]
+})
+export class CoreModule {
+  constructor(
+    @Optional()
+    @SkipSelf()
+    parentModule: CoreModule
+  ) {
+    // Import guard
+    if (parentModule) {
+      throw new Error(`${parentModule} has already been loaded. Import Core module in the AppModule only.`);
+    }
+  }
+}
